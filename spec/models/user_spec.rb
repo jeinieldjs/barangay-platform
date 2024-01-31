dbd83cfd397ea80c4808c6671527ed5f41b8693a
jeinieldjs <silvestrejeiniel@gmail.com>
2024-01-31 14:46:32 +0800

    Added user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:complete_address) }
+    it { should validate_presence_of(:province) }
+    it { should validate_presence_of(:city_municipality) }
+    it { should validate_presence_of(:barangay) }
+    it { should validate_presence_of(:role) }
+    it { should validate_inclusion_of(:role).in_array(['admin', 'resident']) }
+  end
 end
