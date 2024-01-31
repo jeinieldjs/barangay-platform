092333e468f5a7d409fe5497ba2cf4bcf56ec560
jeinieldjs <silvestrejeiniel@gmail.com>
2024-01-31 16:02:07 +0800

    Created API wrapper for PSGC

---
+++ app/api/PSGC/client.rb
@@ -0,0 +1,17 @@
+require_relative 'request'
+
+module PSGC
+    class Client
+        def self.fetch_province
+            response = Request.call(http_method: 'get', endpoint: "/provinces/" )
+        end
+
+        def self.fetch_cities_municipalities(province_code)
+            response = Request.call(http_method: 'get', endpoint: "/provinces/#{province_code}/cities-municipalities" )
+        end
+
+        def self.fetch_barangays(city_or_municipality_code)
+            response = Request.call(http_method: 'get', endpoint: "/cities-municipalities/#{city_or_municipality_code}/barangays" )
+        end
+    end
+end
