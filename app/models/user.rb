62c9b48229619524f7866fe0bcc62871dd210114
jeinieldjs <silvestrejeiniel@gmail.com>
2024-02-05 19:26:39 +0800

    Added announcement model and corresponding test.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
   validates :first_name, :last_name, :email, :complete_address, :province, :city_municipality, :barangay, :role, :status, presence: true
   enum role: { admin: 'admin', resident: 'resident' }
   enum status: { pending: 'pending', approved: 'approved', rejected: 'rejected' }
+
+  has_many :announcements
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_31_112522) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_05_104923) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "announcements", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_announcements_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -34,4 +43,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_31_112522) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "announcements", "users"
 end
