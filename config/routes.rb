e8382d5a470f38251536bd3cf2f5b22f175bd60a
jeinieldjs <silvestrejeiniel@gmail.com>
2024-02-13 16:05:30 +0800

    Added posts on admin's end.

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
     
     resources :announcements
     resources :complaints, only: [:index, :edit, :update, :show]
+    resources :posts
   end
 
   namespace :resident do
