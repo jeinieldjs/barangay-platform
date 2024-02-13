8c61bcc5f95a46ef5d0bf061d93990f5c8ddb791
jeinieldjs <silvestrejeiniel@gmail.com>
2024-02-13 15:22:44 +0800

    Added search bar on admin residents index. Added styles for overflow.

--- app/controllers/admin/residents_controller.rb
+++ app/controllers/admin/residents_controller.rb
@@ -10,6 +10,9 @@ class Admin::ResidentsController < ApplicationController
               city_municipality: current_user.city_municipality,
               province: current_user.province
               )
+      if params[:last_name_query].present?
+        @users = @users.where("LOWER(last_name) LIKE ?", "%#{params[:last_name_query].downcase}%")
+      end
     end
 
     def pending
