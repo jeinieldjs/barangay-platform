f0e61bd2c042333b196f17cab81f3b389a20619b
jeinieldjs <silvestrejeiniel@gmail.com>
2024-02-17 10:35:16 +0800

    Added model tests for post, likes and comments.

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,5 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:content) }
+  end
+
+  describe 'associations' do
+    it { should belong_to(:user) }
+    it { should belong_to(:post) }
+  end
 end
 
