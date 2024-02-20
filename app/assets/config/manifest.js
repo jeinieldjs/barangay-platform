783ca217ee890c57aec393f6015305ea582e7084
jeinieldjs <silvestrejeiniel@gmail.com>
2024-02-21 00:52:37 +0800

    update manifest.js config

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,5 @@
 //= link_tree ../images
-//= link ../stylesheets/application.css
+//= link_tree ../stylesheets
 //= link_tree ../../../vendor/javascript .js
 //= link likes.js
 //= link rails-ujs.js
